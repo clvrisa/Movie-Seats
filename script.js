805df9b346bbe4528c1e2010a2cf0617ec82a2bf
Clarisa Zapata <czapata@pop-os.localdomain>
2021-12-21 19:49:54 -0600

    add event listener on the container, then create an updated selected count function to display current total and current price

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  
+    const selectedSeatsCount = selectedSeats.length;
+  
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+    
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
+  }
+
+container.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')
+    ) {
+      e.target.classList.toggle('selected');
+  
+      updateSelectedCount();
+    }
+  });
+  
