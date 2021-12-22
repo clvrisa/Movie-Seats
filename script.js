aab7d2e0e4b7e1e467aa716ae284dcb51aa629be
Clarisa Zapata <czapata@pop-os.localdomain>
2021-12-22 10:29:12 -0600

    update to arrow functions, add local storage

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
-function updateSelectedCount() {
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//update total and count
+const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
   
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-  
+
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-  
+
     const selectedSeatsCount = selectedSeats.length;
   
     count.innerText = selectedSeatsCount;
@@ -21,6 +27,14 @@ function updateSelectedCount() {
     setMovieData(movieSelect.selectedIndex, movieSelect.value);
   }
 
+//movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+})
+
+ //seat click event listener 
 container.addEventListener('click', e => {
     if (
       e.target.classList.contains('seat') &&
